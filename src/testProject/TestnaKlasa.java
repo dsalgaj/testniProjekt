ea2207ea937185ff054d7713c26ba614ffba463d
SilvioJavorek <silvio.javorek@vuv.hr>
2022-04-07 15:32:35 +0200

    Prvi commit

---
+++ src/testProject/TestnaKlasa.java
@@ -0,0 +1,10 @@
+package testProject;
+
+public class TestnaKlasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testiranje instalacije je uspjesno!");
+	}
+
+}
